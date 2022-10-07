fef8b2158f31bff15bd7833379ef253f7beda706
kei-kmj <82737807+kei-kmj@users.noreply.github.com>
2022-10-07 09:09:58 +0900

    hello

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Hello from './components/Hello'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Hello name="React" />
+  </React.StrictMode>
+)
